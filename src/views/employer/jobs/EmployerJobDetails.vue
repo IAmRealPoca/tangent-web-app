af3e762a034347881e4e298b458c1287cf59004d
Ngo Son Nam <44229649+IAmRealPoca@users.noreply.github.com>
2021-02-24 03:35:18 +0700

    no message

---
+++ src/views/employer/jobs/EmployerJobDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <MainContent> </MainContent>
+  </div>
+</template>
+
+<script>
+import MainContent from "@/components/MainContent.vue";
+export default {
+  name: "EmployerJobDetails",
+  components: {
+    MainContent,
+  },
+};
+</script>
+
+<style scoped>
+</style>
