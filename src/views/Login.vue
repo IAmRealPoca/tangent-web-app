42a1ed94a17eeb8d3de8328961b871475d75c62e
Thông Thái <46261937+nikethai@users.noreply.github.com>
2021-02-26 14:20:28 +0700

    realign button + fix select dropdown

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -29,39 +29,51 @@
                 <div class="text-center text-md-center mb-4 mt-md-0">
                   <h1 class="mb-0 h3">Sign in to our platform</h1>
                 </div>
-                <div class="btn-group me-2 mb-2">
-                  <button type="button" class="btn btn-primary">
-                    {{ selectedAccTypeString }}
-                  </button>
-                  <button
-                    type="button"
-                    class="btn btn-primary dropdown-toggle dropdown-toggle-split"
-                    data-bs-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    <i class="fas fa-angle-down dropdown-arrow"></i>
-                    <span class="sr-only">\/</span>
-                  </button>
-                  <div class="dropdown-menu">
-                    <div
-                      class="dropdown-item"
-                      @click="selectedAccTypeId = accType.typeId"
-                      v-for="(accType, index) in listOfAccountType"
-                      :key="index"
+                <div class="d-flex justify-content-center my-4">
+                  <div class="btn-group mx-auto">
+                    <button
+                      type="button"
+                      class="btn btn-primary dropdown-toggle"
+                      data-bs-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
                     >
-                      {{ accType.typeName }}
+                      {{ selectedAccTypeString }}
+                      <i class="fas fa-angle-down dropdown-arrow"></i>
+                      <span class="sr-only">\/</span>
+                    </button>
+                    <!-- <button
+                      type="button"
+                      class="btn btn-primary dropdown-toggle"
+                      data-bs-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      <i class="fas fa-angle-down dropdown-arrow"></i>
+                      <span class="sr-only">\/</span>
+                    </button> -->
+                    <div class="dropdown-menu m-0">
+                      <div
+                        class="dropdown-item"
+                        v-for="(accType, index) in listOfAccountType"
+                        @click="selectType(accType.typeId)"
+                        data-bs-toggle="tooltip"
+                        :key="index"
+                      >
+                        {{ accType.typeName }}
+                      </div>
                     </div>
                   </div>
                 </div>
-                <span
-                    class="fas text-danger ms-2"
-                    title=""
-                    data-bs-toggle="tooltip"
-                  ></span>
+
+                <!-- <span
+                  class="fas text-danger ms-2"
+                  title=""
+                  data-bs-toggle="tooltip"
+                ></span> -->
                 <div class="d-flex justify-content-center my-4">
                   <button
-                    class="btn btn-google me-3 mb-3"
+                    class="btn btn-google mx-auto"
                     type="button"
                     @click="onClick"
                   >
@@ -129,6 +141,25 @@ export default {
     });
 
     selectedAccTypeString.value = "[Select type]";
+
+    const selectType = (id) => {
+      console.log("id",id);
+      if (id === 0 || id === 1 || id === 2 || id === 4) {
+        if (id=== 0) {
+          selectedAccTypeString.value = "Admin";
+        }
+        if (id=== 1) {
+          selectedAccTypeString.value = "Employee";
+        }
+        if (id=== 2) {
+          selectedAccTypeString.value = "Employer";
+        }
+        if (id=== 4) {
+          selectedAccTypeString.value = "School";
+        }
+      }
+    };
+
     watchEffect((selectedAccTypeId) => {
       if (selectedAccTypeId.value === 0) {
         selectedAccTypeString.value = "Admin";
@@ -163,17 +194,15 @@ export default {
 
     const fetchUserData = (usrFlag) => {
       if (usrFlag === 0) {
-
       } else if (usrFlag === 2) {
-        employerService.getCurrEmployerInfo()
-        .then((resp) => {
+        employerService.getCurrEmployerInfo().then((resp) => {
           console.log(resp);
           sessionStorage.setItem("userInfo", JSON.stringify(resp));
         });
       } else if (usrFlag === 4) {
         // schoolService.
       }
-    }
+    };
 
     const onChange = () => {
       console.log("Normal trigger");
@@ -196,10 +225,10 @@ export default {
       onClick,
       onChange,
       onClickToOpenVidu,
+      selectType
     };
   },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
