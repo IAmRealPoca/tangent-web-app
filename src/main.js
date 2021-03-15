8c9d8f53d41982526dcea9fcdbc230469b518285
Thông Thái <46261937+nikethai@users.noreply.github.com>
2021-03-15 10:36:34 +0700

    add test lodash

--- src/main.js
+++ src/main.js
@@ -21,6 +21,9 @@ const firebaseConfig = {
   appId: "1:596123512687:web:0e2c4583dc46ec02f0029a",
 };
 
+// let messaging: firebase.messaging.Messaging; 
+// if (firebase.messaging.isSupported()) { messaging = firebase.messaging(); } export { messaging };
+
 firebase.initializeApp(firebaseConfig);
 
 const app = createApp(App);
