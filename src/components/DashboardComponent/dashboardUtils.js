4aa2e58181827d0a4646b1b339832e91028ecc52
Quy Nguyen <steve.parker117@gmail.com>
2021-03-21 19:40:38 +0700

    #load full function check time on EmployeeDashboard

--- src/components/DashboardComponent/dashboardUtils.js
+++ src/components/DashboardComponent/dashboardUtils.js
@@ -1,5 +1,11 @@
 export const CSS_BUTTON_COLOR = {
-  EXPIRED: 'text-success',
-  NEAR_EXPIRED: 'text-warning',
-  OVER_EXPIRED: 'text-danger',
-};
+  EXPIRED: 'badge bg-danger text-wrap text-white',
+  NEAR_EXPIRED: 'badge bg-warning text-wrap text-dark',
+  LONG_EXPIRED: 'badge bg-success text-wrap text-white',
+}
+
+export const LABELS = {
+  EXPIRED: "Đã hết hạn",
+  NEAR_EXPIRED: "Còn lại",
+  LONG_EXPIRED: "Hết hạn"
+}
